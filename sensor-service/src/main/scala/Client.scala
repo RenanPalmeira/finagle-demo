9b1aa288d213a680c8e39b0f9d4076069e10e4d7
Renan Palmeira <renanpalmeira1@hotmail.com>
2017-07-02 10:06:57 -0300

    Initial client implementation of thrift with Finagle

---
+++ sensor-service/src/main/scala/Client.scala
@@ -0,0 +1,15 @@
+package com.temperature.sensor
+
+import com.twitter.finagle.Thrift
+import com.twitter.util.{Await, Future}
+
+import com.temperature.thrift.{ TemperatureService }
+
+object Client extends App {
+    val client = Thrift.client.newIface[TemperatureService.FutureIface]("localhost:8080")
+    
+    val display = client.hi()
+        .onSuccess({ response =>cprintln("Received response: " + response) })
+
+    Await.result(display)
+}
