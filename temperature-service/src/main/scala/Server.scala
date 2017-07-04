b37c966494451229701cf7cb94912a8482819a18
Renan <tomrpg44@gmail.com>
2017-07-04 12:59:12 -0300

    Update Server.scala

--- temperature-service/src/main/scala/Server.scala
+++ temperature-service/src/main/scala/Server.scala
@@ -11,22 +11,22 @@ object Server extends App {
         def hi() = Future.value("hi")
     }
 
-// scrooge generates class to wrap a thrift service into a finagle compatible service definition
-val finagledService = new TemperatureService.FinagledService(
-  service, Protocols.binaryFactory()
-)
+    // scrooge generates class to wrap a thrift service into a finagle compatible service definition
+    val finagledService = new TemperatureService.FinagledService(
+      service, Protocols.binaryFactory()
+    )
 
-// run and announce the server
- val server = Thrift.server
-      .withLabel("temperature-service")
-      .serveAndAnnounce(
-        // static announcemnt via zookeeper
-        name = "zk!127.0.0.1:2181!/service/temperature!0",
-        // bind this service to this address
-        addr = ":8082",
-        // the service to announce
-        service = finagledService
-)
+    // run and announce the server
+     val server = Thrift.server
+          .withLabel("temperature-service")
+          .serveAndAnnounce(
+            // static announcemnt via zookeeper
+            name = "zk!127.0.0.1:2181!/service/temperature!0",
+            // bind this service to this address
+            addr = ":8082",
+            // the service to announce
+            service = finagledService
+          )
 
     Await.ready(server)
 }
