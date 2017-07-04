5cc92627d9ead641c0b9486aaa0c0a238559787b
Renan Palmeira <renanpalmeira1@hotmail.com>
2017-07-03 23:57:14 -0300

    Initial use Zookeeper

--- temperature-service/src/main/scala/Server.scala
+++ temperature-service/src/main/scala/Server.scala
@@ -6,11 +6,14 @@ import com.twitter.util.{Await, Future}
 import com.temperature.thrift.{ TemperatureService }
 
 object Server extends App {
-    val server = Thrift.server.serveIface("localhost:8080", new TemperatureService[Future] {
-        def hi() = {
-            Future.value("hi")
-        }
-    })
+    val service = new TemperatureService[Future] {
+        def hi() = Future.value("hi")
+    }
+
+    val server = Thrift.server
+        .withLabel("temperature-service")
+        .serveIface("localhost:8080", service)
+        .announce("zk!127.0.0.1:2181!/service/temperature!0")
 
     Await.ready(server)
 }
