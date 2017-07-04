bfba7f466a104f72135d9e4ec24cf695a2d2295a
Renan Palmeira <renanpalmeira1@hotmail.com>
2017-07-04 10:11:06 -0300

    Fixed error in using zookeeper

--- temperature-service/src/main/scala/Server.scala
+++ temperature-service/src/main/scala/Server.scala
@@ -1,19 +1,32 @@
 package com.temperature.server
 
 import com.twitter.finagle.Thrift
+import com.twitter.finagle.thrift.Protocols
 import com.twitter.util.{Await, Future}
 
 import com.temperature.thrift.{ TemperatureService }
 
 object Server extends App {
-    val service = new TemperatureService[Future] {
+    val service = new TemperatureService.FutureIface {
         def hi() = Future.value("hi")
     }
 
-    val server = Thrift.server
-        .withLabel("temperature-service")
-        .serveIface("localhost:8080", service)
-        .announce("zk!127.0.0.1:2181!/service/temperature!0")
+// scrooge generates class to wrap a thrift service into a finagle compatible service definition
+val finagledService = new TemperatureService.FinagledService(
+  service, Protocols.binaryFactory()
+)
+
+// run and announce the server
+ val server = Thrift.server
+      .withLabel("temperature-service")
+      .serveAndAnnounce(
+        // static announcemnt via zookeeper
+        name = "zk!127.0.0.1:2181!/service/temperature!0",
+        // bind this service to this address
+        addr = ":8082",
+        // the service to announce
+        service = finagledService
+)
 
     Await.ready(server)
 }
